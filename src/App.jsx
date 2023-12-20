99533ef0604f689dbd2081b4c9ec904a7c0ffdc9
anbumani12 <anbumani15012002@gmail.com>
2023-12-20 18:32:43 +0530

    code completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-
+import Card from './components/Card'
 function App() {
   let data = [
     {
       title: "Project one",
-      description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque earum nostrum suscipit ducimus nihil provident, perferendis rem illo, voluptate atque, sit eius in voluptates, nemo repellat fugiat excepturi! Nemo, esse.`
+      description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque earum nostrum suscipit ducimus nihil provident, sit eius in voluptates, nemo repellat fugiat excepturi! Nemo, esse.`
     },
     {
       title: "Project two",
-      description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque earum nostrum suscipit ducimus nihil provident, perferendis rem illo, voluptate atque, sit eius in voluptates, nemo repellat fugiat excepturi! Nemo, esse.`
+      description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit,perferendis rem illo, voluptate atque, sit eius in voluptates, nemo repellat fugiat excepturi! Nemo, esse.`
     },
     {
       title: "Project three",
-      description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque earum nostrum suscipit ducimus nihil provident, perferendis rem illo, voluptate atque, sit eius in voluptates, nemo repellat fugiat excepturi! Nemo, esse.`
+      description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque earum nostrum suscipit ducimus nihil provident nemo repellat fugiat excepturi! Nemo, esse.`
     },
     {
       title: "Project four",
-      description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque earum nostrum suscipit ducimus nihil provident, perferendis rem illo, voluptate atque, sit eius in voluptates, nemo repellat fugiat excepturi! Nemo, esse.`
+      description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque earum nostrum suscipit ducimus nihil provident,  nemo repellat fugiat excepturi! Nemo, esse.`
     },
     {
       title: "Project five",
@@ -24,35 +24,26 @@ function App() {
     },
     {
       title: "Project six",
-      description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque earum nostrum suscipit ducimus nihil provident, perferendis rem illo, voluptate atque, sit eius in voluptates, nemo repellat fugiat excepturi! Nemo, esse.`
+      description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, perferendis rem illo, voluptate atque, sit eius in voluptates, nemo repellat fugiat excepturi! Nemo, esse.`
     },
   ];
 
-  return (
-    <>
+  return <>
+    
       <div className="container">
         <h1 className="my-4">Page Heading
           <small>Secondary Text</small>
         </h1>
 
         <div className="row">
-          {data.map((e, i) => (
-            <div className="col-lg-4 col-sm-6 mb-4" key={i}>
-              <div className="card h-100">
-                <a href="#"><img className="card-img-top" src="https://via.placeholder.com/700x400" alt="" /></a>
-                <div className="card-body">
-                  <h4 className="card-title">
-                    <a href="#">{e.title}</a>
-                  </h4>
-                  <p className="card-text">{e.description}</p>
-                </div>
-              </div>
-            </div>
-          ))}
+          {
+          data.map((e, i) => {
+            return <Card cardData={e} key={i}/>
+          })
+        }     
         </div>
       </div>
     </>
-  );
 }
 
 export default App;
